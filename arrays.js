4fecce4ec0db34440510d02cdc67e4cebd1d2331
A Lee <ayslee@gmail.com>
2018-01-14 04:13:26 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -4,35 +4,35 @@ var chocolateBars = [
   "kitkat",
   "skittles"
   ];
-console.log("chocolateBars " + chocolateBars);
+console.log("chocolateBars ->" + chocolateBars);
 //-------------------------------
 function addElementToBeginningOfArray(array, element) {
   var newArray = [element, ...array];
   return (newArray);
 }
-console.log("addElement " + addElementToBeginningOfArray([1], "foo"));
+console.log("addElement ->" + addElementToBeginningOfArray([1], "foo"));
 //--------------------------------
 function destructivelyAddElementToBeginningOfArray(array, element) {
   var x = array
   return(x.unshift(element));  
 }
-console.log("destructivelyAdd " + destructivelyAddElementToBeginningOfArray([1], "foo"));
+console.log("destructivelyAdd ->" + destructivelyAddElementToBeginningOfArray([1], "foo"));
 //---------------------------------
 function addElementToEndOfArray(array, element) {
   var newArray2 = [array, ...element];
   return newArray2;
 }
-console.log("addElementToEnd " + addElementToEndOfArray([1], "foo"));
+console.log("addElementToEnd ->" + addElementToEndOfArray([1], "foo"));
 //---------------------------------
 function destructivelyAddElementToEndOfArray(array, element) {
   return(array.push(element));
 }
-console.log("destrAddEnd " + destructivelyAddElementToEndOfArray([1], "foo"));
+console.log("destrAddEnd ->" + destructivelyAddElementToEndOfArray([1], "foo"));
 //---------------------------------
 function accessElementInArray(array, i) {
   return(array[i]);
 }
-console.log("accessElementInArray " + accessElementInArray([1, 2, 3], 2));
+console.log("accessElementInArray ->" + accessElementInArray([1, 2, 3], 2));
 //---------------------------------
 
 
